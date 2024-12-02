86f304c37838bd1a7cb7f961c795ea3d41da7a95
Austin Nhung <austinnhung@yahoo.ca>
2024-12-01 21:40:29 -0700

    Finished second part of day 1

--- day1/solution.go
+++ day1/solution.go
@@ -10,18 +10,9 @@ import (
 )
 
 
-func main() {
-    data, err := os.ReadFile("input.txt")
-
-    if err != nil {
-        panic(err)
-    }
-
-    rows := strings.Split(string(data), "\n")
-
+func load_rows(rows []string) ([]int, []int) {
     var left_col []int
     var right_col []int
-    var n_rows int = 0
 
     for _, row := range rows {
         items := strings.Fields(row)
@@ -44,13 +35,19 @@ func main() {
 
         left_col = append(left_col, left_item)
         right_col = append(right_col, right_item)
-        n_rows += 1
     }
 
+    return left_col, right_col
+}
+
+
+func part_1(left_col []int, right_col []int) int {
+    // Sort columns and calculate distance between sorted columns
     sort.Ints(left_col)
     sort.Ints(right_col)
 
     var total_distance int = 0
+    var n_rows int = len(left_col)
 
     for i := 0; i < n_rows; i++ {
         var distance int = left_col[i] - right_col[i]
@@ -62,5 +59,50 @@ func main() {
         total_distance += distance
     }
 
-    fmt.Println(total_distance)
+    return total_distance
+}
+
+
+func part_2(left_col []int, right_col []int) int {
+    // Calculate the similarity score between the columns, equal to the number
+    // of times a number in the left column appears in the right column
+    
+    // Find the number of times a number appears in the right column
+    factor := make(map[int]int)
+
+    for _, value := range right_col {
+        existing := factor[value]
+        factor[value] = existing + 1
+    }
+
+    // Calculate the similarity score
+    var similarity int = 0
+
+    for _, value := range left_col {
+        n, ok := factor[value]
+
+        if ok {
+            similarity += value * n
+        }
+    }
+
+    return similarity
+}
+
+
+func main() {
+    data, err := os.ReadFile("input.txt")
+
+    if err != nil {
+        panic(err)
+    }
+
+    rows := strings.Split(string(data), "\n")
+    left_col, right_col := load_rows(rows)
+
+    part_1 := part_1(left_col, right_col)
+    part_2 := part_2(left_col, right_col)
+
+    fmt.Println(part_1)
+    fmt.Println(part_2)
 }
